16023dfae403846ead4401fbf973605bd2979da3
lsrong <lsrong0414@gmail.com>
2020-08-04 14:42:55 +0800

    feature:hello world

--- step1/hello/main.go
+++ step1/hello/main.go
@@ -5,9 +5,9 @@ import (
 	"time"
 )
 
-func main()  {
+func main() {
 	for i := 1; i <= 3; i++ {
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Println("hello world  ", i, " times")
 	}
 }
