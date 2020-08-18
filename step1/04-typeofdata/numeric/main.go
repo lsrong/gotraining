0332e62385c9783fda168b73b6459d18ff391288
lsrong <lsrong0414@gmail.com>
2020-08-18 18:06:25 +0800

    feature:type of data numeric and string

---
+++ step1/04-typeofdata/numeric/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	/**
+	1.整型:
+	有符号:
+	int     32位系统占4字节（与int32范围一样），64位系统占8个节（与int64范围一样）
+	int8    占据1字节   范围 -128 ~ 127
+	int16   占据2字节   范围 -2(15次方) ~ 2（15次方）-1
+	int32   占据4字节   范围 -2(31次方) ~ 2（31次方）-1
+	int64   占据8字节   范围 -2(63次方) ~ 2（63次方）-1
+	rune	int32的别称
+
+	无符号
+	uint	32位系统占4字节（与uint32范围一样），64位系统占8字节（与uint64范围一样）
+	uint8   占据1字节   范围 0 ~ 255
+	uint16  占据2字节   范围 0 ~ 2（16次方）-1
+	uint32  占据4字节   范围 0 ~ 2（32次方）-1
+	uint64  占据8字节   范围 0 ~ 2（64次方）-1
+	byte	uint8的别称
+	*/
+
+	/**
+	2.浮点类型:
+	float32 单精度  占据4字节   范围 -3.403E38 ~ 3.403E38    (math.MaxFloat32)
+	float64 双精度  占据8字节   范围 -1.798E208 ~ 1.798E308  (math.MaxFloat64)
+	*/
+	fmt.Println(math.MaxFloat32)
+
+	fmt.Println(math.MaxFloat64)
+
+	/**
+	注意:精度缺失
+	var num1 float32 = -123.0000901
+	var num2 float64 = -123.0000901
+	fmt.Println("num1=",num1)		// -123.00009
+	fmt.Println("num2=",num2)		// -123.0000901
+	*/
+	var num1 float64 = -123.0000901
+	var num2 float64 = -123.000901
+	fmt.Println("num1=", num1) // -123.00009
+	fmt.Println("num2=", num2) // -123.0000901
+	fmt.Println(isFloatEqual(num1, num2, 0.0001))
+
+	/**
+	3.NaN
+	*/
+	nan := math.NaN()
+	fmt.Println(math.IsNaN(nan))
+}
+
+func isFloatEqual(f1, f2, p float64) bool {
+	return math.Abs(f1-f2) < p
+}
---
+++ step1/04-typeofdata/string/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	/**
+	单个字符(字母)，一般使用 byte 来保存，且使用单引号包裹
+	*/
+
+	/**
+	1.格式化输出:
+	%%	%字面量
+	%b	二进制整数值，基数为2，或者是一个科学记数法表示的指数为2的浮点数
+	%c	该值对应的unicode字符
+	%d	十进制数值，基数为10
+	%e	科学记数法e表示的浮点或者复数
+	%E	科学记数法E表示的浮点或者附属
+	%f	标准计数法表示的浮点或者附属
+	%o	8进制度
+	%p	十六进制表示的一个地址值
+	%s	输出字符串或字节数组
+	%T	输出值的类型，注意int32和int是两种不同的类型，编译器不会自动转换，需要类型转换。
+	%v	值的默认格式表示
+	%+v	类似%v，但输出结构体时会添加字段名
+	%#v	值的Go语法表示
+	%t	单词true或false
+	%q	该值对应的单引号括起来的go语法字符字面值，必要时会采用安全的转义表示
+	%x	表示为十六进制，使用a-f
+	%X	表示为十六进制，使用A-F
+	%U	表示为Unicode格式：U+1234，等价于"U+%04X"
+	*/
+
+	/**
+	1.Go的字符串是由单个字节连接起来的，即Go字符串是一串固定长度的字符连接起来的字符序列,内容初始化后不能修改
+	一个中文3字节
+	*/
+	var str1 string
+	str1 = "tests "
+	str2 := "中文"
+
+	fmt.Println(str1[0])
+	fmt.Println(len(str2))
+	fmt.Println(str1 + str2)
+	/**
+	修改字符
+	*/
+	// 字节数组[]byte类型 构建临时字符串
+	var str string = "hello"
+	strTemp := []byte(str)
+	fmt.Println(strTemp)
+
+	strTemp[0] = 'c'
+	strChange := string(strTemp)
+	fmt.Println(strChange)
+
+	// 切片:str[1:], 从1开始到最后一位
+	test := "test"
+	fmt.Println("he" + test[2:])
+
+	/**
+	3.常用的string函数
+	*/
+	// len():长度(字节)
+	lenStr1 := "test"
+	lenStr2 := "中文"
+	fmt.Println(len(lenStr1))
+	fmt.Println(len(lenStr2))
+	// 统一字符的UTF-8格式长度,字符长度
+	fmt.Println(utf8.RuneCountInString(lenStr2))
+	// 字符遍历
+	for i := 0; i < len(lenStr1); i++ { //for
+		fmt.Println(i, string(lenStr1[i]))
+	}
+	for i, ch := range lenStr2 { // for range
+		fmt.Println(i, string(ch))
+	}
+
+	// string():字符串转换
+	num := 1
+	fmt.Printf("%T", num)
+	fmt.Printf("%T", string(num))
+	fmt.Println("")
+
+	// 使用+能够连接字符串。但是该操作并不高效（因为字符串在Go中是基本类型，每次拼接都是拷贝了内存！）。
+	// Go1.10提供了类似Java的StringBuilder机制来进行高效字符串连接
+	strBuilder1 := "hello "
+	strBuilder2 := "world"
+	fmt.Println(splice(strBuilder1, strBuilder2))
+
+	//strings 包相关函数
+	fmt.Println(strings.Index(str1, "s")) //索引
+
+}
+
+// bytes.Buffer 拼接字符串
+func splice(str1 string, str2 string) string {
+	var stringBuilder bytes.Buffer
+	stringBuilder.WriteString(str1)
+	stringBuilder.WriteString(str2)
+
+	return stringBuilder.String()
+}
