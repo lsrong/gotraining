da9d93a6e1204cbfe72df84e17034b3e3222eb21
lsrong <lsrong44@gmail.com>
2022-01-12 14:14:37 +0800

    15 puzzle game implement

--- tasks/15_puzzle_game/main.go
+++ tasks/15_puzzle_game/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 type board [16]cell
 type cell uint8
 type move uint8
 
+// String 打印4*4宫格
 func (b *board) String() string {
 	var s strings.Builder
 	for i, c := range b {
@@ -49,23 +56,31 @@ type puzzle struct {
 	level int
 }
 
+// newPuzzle 创建一个新迷宫游戏对象
 func newPuzzle(level uint) *puzzle {
 	p := puzzle{
 		board: solvedBoard,
 		empty: 15,
 	}
 
-	// 初始化
+	// 初始化: 10-easy, 50-normal,100-hard
 	p.shuffle(level)
 	p.moves -= int(level)
 
 	return &p
 }
 
+// shuffle 打乱顺序,初始化游戏难度.
 func (p *puzzle) shuffle(moves uint) {
-
+	for i := 0; i < int(moves) || p.board == solvedBoard; {
+		m := move(rand.Intn(4))
+		if p.move(m) {
+			i++
+		}
+	}
 }
 
+// move 移动一次步伐
 func (p *puzzle) move(m move) bool {
 	emptyIndex := p.empty
 	newIndex, ok := p.isValidMove(m)
@@ -98,10 +113,62 @@ func (p *puzzle) isValidMove(m move) (int, bool) {
 	}
 }
 
-func (p *puzzle) play() {
+// playOneMove 移动一次操作
+func (p *puzzle) playOneMove() {
+	for {
+		fmt.Printf("Please Move #%d (U-up, D-down, L-left, R-right, Q-quit): ", p.moves+1)
+		// 读取标准输入的一个字符
+		var s string
+		if n, err := fmt.Scanln(&s); err != nil || n != 1 {
+			continue
+		}
 
+		s = strings.TrimSpace(s)
+		if s == "" {
+			continue
+		}
+		var m move
+		switch s[0] {
+		case 'U', 'u':
+			m = up
+		case 'd', 'D':
+			m = down
+		case 'l', 'L':
+			m = left
+		case 'r', 'R':
+			m = right
+		default:
+			fmt.Println(`
+Please enter "U", "D", "L", or "R" to move the # cell
+up, down, left, or right. You can also enter "Q" to quit.
+Upper or lowercase is accepted and only the first non-blank
+character is important (i.e. you may enter "up" if you like).`)
+			continue
+
+		}
+		if !p.move(m) {
+			fmt.Println("That is not a valid move at the moment.")
+		}
+		return
+	}
+}
+
+// play 开始游戏
+func (p *puzzle) play() {
+	fmt.Println("Starting puzzle...")
+	// 执行一次移动
+	for p.board != solvedBoard && !p.quit {
+		fmt.Printf("\n%+v\n", &p.board)
+		p.playOneMove()
+	}
+
+	// 结束判断是否成功解决15-puzzle
+	if p.board == solvedBoard {
+		fmt.Printf("You solved the puzzle in %d moves.\n", p.moves)
+	}
 }
 
 func main() {
-	fmt.Print(solvedBoard.String(), easy, normal, hard)
+	p := newPuzzle(normal)
+	p.play()
 }
---
+++ tasks/15_puzzle_game/readme.md
@@ -0,0 +1,7 @@
+# 15 puzzle game
+## Task
+Implement the Fifteen Puzzle Game.  
+实现十五益智游戏。
+
+![Ardan Labs](300px-15_puzzle.png)
+
